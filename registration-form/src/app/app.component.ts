0a1d76550fb40b61d6c0cd35f16a68354bbf1eaa
Kanak Prajapati <kn436428@dal.ca>
2021-11-03 20:56:22 -0300

    Password Confirmation

--- registration-form/src/app/app.component.ts
+++ registration-form/src/app/app.component.ts
@@ -1,7 +1,7 @@
+import { registerLocaleData } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-// import { ConfirmedValidator } from './confirmed.validator';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,27 +15,26 @@ export class AppComponent implements OnInit {
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
-    this.registrationForm = this.fb.group(
-      {
-        email: ['', [Validators.required, Validators.email]],
-        username: ['', [Validators.required]],
-        password: [
-          '',
-          [
-            Validators.required,
-            Validators.pattern(
-              '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[ -/:-@[-`{-~]).{6,64}$'
-            ), // Pattern Source - https://stackoverflow.com/questions/30299464/pattern-password-javascript
-            Validators.min(8),
-          ],
+    this.registrationForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      username: [
+        '',
+        [Validators.required],
+        Validators.pattern('^[A-Za-z][A-Za-z0-9_]$'), // Pattern Source- https://laasyasettyblog.hashnode.dev/validating-username-using-regex
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[ -/:-@[-`{-~]).{6,64}$'
+          ), // Pattern Source - https://stackoverflow.com/questions/30299464/pattern-password-javascript
+          Validators.min(8),
         ],
-        confirmPassword: ['', [Validators.required]],
-      }
-      // {
-      //   validator: ConfirmedValidator('password', 'confirm_password'),
-      // }
-    );
-    this.registrationForm.valueChanges.subscribe(console.log);
+      ],
+      confirmPassword: ['', [Validators.required]],
+    });
+    // this.registrationForm.valueChanges.subscribe(console.log);
   }
   // Reference - https://jasonwatmore.com/post/2019/05/22/angular-7-tutorial-part-5-registration-form-user-service
   get Register() {
@@ -43,13 +42,12 @@ export class AppComponent implements OnInit {
   }
 
   onSubmit() {
+    console.log(this.registrationForm.value);
     this.submitted = true;
     if (this.registrationForm.invalid) {
       return;
     }
-    alert(
-      'You have successfully logged in!' +
-        JSON.stringify(this.registrationForm.value, null, 4)
-    );
+
+    alert('You have successfully logged in!');
   }
 }
